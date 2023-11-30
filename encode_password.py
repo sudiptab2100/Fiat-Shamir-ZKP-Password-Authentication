b101dd71feb4ab62d8843020a909f8fbda11f9c8
Sudipta Basak <sudiptab2100@gmail.com>
2023-11-30 17:03:31 +0530

    Implemented SHA256 hash based encoding

--- encode_password.py
+++ encode_password.py
@@ -3,5 +3,4 @@ from hashlib import sha256
 def encode_password(password):
     ep = password.encode('utf-8')
     hp = sha256(ep).hexdigest()
-    bp = int.from_bytes(ep, byteorder='big')
-    return bp
+    return int(hp, 16)
